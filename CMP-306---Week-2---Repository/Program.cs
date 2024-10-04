43dff72219bc666d318aaeba4df6f8986c6793a3
gibhubSigma <2200688@uad.ac.uk>
2024-10-04 12:08:13 +0100

    Add calculator class

--- CMP-306---Week-2---Repository/Program.cs
+++ CMP-306---Week-2---Repository/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("I just added this to visual studio");
+Console.WriteLine("I just added this to the command line repo");
+Console.WriteLine("This line added to show the staging area");
